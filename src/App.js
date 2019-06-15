756f0f4e1df0239e2506839e6c3c8fc21cdc5ea3
Bruno Cardoso <bruno@liveonsolutions.com>
2019-06-15 20:48:32 -0300

    creating header and the style, creating the feed page and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Routes from "./routes";
+import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,73 @@
 import React, { Component } from "react";
 
-// import { Container } from './styles';
+import "./Feed.css";
+
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
 
 export default class Feed extends Component {
   render() {
-    return <h1>Feed</h1>;
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Bruno Cardoso</span>
+              <span className="place">Jaú</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img
+            src="http://localhost:3333/files/WIN_20190416_10_02_37_Pro.jpg"
+            alt=""
+          />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="Curtir" />
+              <img src={comment} alt="Comentar" />
+              <img src={send} alt="Enviar" />
+            </div>
+            <strong>900</strong>
+            <p>
+              Um post muito massa da ominiStack!
+              <span>#react #reactnative #nodejs</span>
+            </p>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Bruno Cardoso</span>
+              <span className="place">Jaú</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img
+            src="http://localhost:3333/files/WIN_20190416_10_02_37_Pro.jpg"
+            alt=""
+          />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="Curtir" />
+              <img src={comment} alt="Comentar" />
+              <img src={send} alt="Enviar" />
+            </div>
+            <strong>900</strong>
+            <p>
+              Um post muito massa da ominiStack!
+              <span>#react #reactnative #nodejs</span>
+            </p>
+          </footer>
+        </article>
+      </section>
+    );
   }
 }
